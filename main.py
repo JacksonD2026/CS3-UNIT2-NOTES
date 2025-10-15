4888863e4241f389a461cb527a2af61aaa3fbb7f
jackson <jackson.dobson2026@gbwl.org>
2025-10-15 13:41:08 +0000

    panda

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("hello world")
+import numpy as np
+import pandas as pd
 
+ # Series object is a 1D array of indexed data
+ # Like a COLUMN (one catagory)
+month = pd.Series(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+print(month)
+print(month.values)
+print(month.index)
 
-if __name__ == "__main__":
-    main()
+# you can set the index explicitly!!!
+betterMonth = pd.Series(month, index=[1,2,3,4,5,6,7,8,9,10,11])
+
+print(f'My birthday is in {betterMonth[7]}ust')
+
+# Can also think of series like a simple dictionary with key:value pairs
+birth_Months = {'Kevin':'Mar',
+                'Jackson' : 'Aug',
+                'Finny':'Jul',
+                'Bryce':'Nov',
+                'Natalie':'Mar',
+                'Paige':'Feb',
+                'Maia':'Apr'}
+birth_series = pd.Series(birth_Months)
+print(birth_series)
